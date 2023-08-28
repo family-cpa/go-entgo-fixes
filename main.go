396e8faae24f127ee293afdfd37ba88668313b40
xjester0 <xjester0@gmail.com>
2023-08-28 13:23:54 +0300

    add remove schema node and nodes query

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"os"
 )
 
+var nodePt = `"""Fetches an object given its ID."""
+  node(
+    """ID of the object."""
+    id: ID!
+  ): Node`
+
+var nodesPt = `"""Lookup nodes by a list of IDs."""
+  nodes(
+    """The list of node IDs."""
+    ids: [ID!]!
+  ): [Node]!`
+
 func main() {
 	path, _ := os.Getwd()
-	log.Fatalf("failed to run fixer in: %s", path)
+	schemaFiles := []string{"/graph/ent.graphql"}
+
+	for _, file := range schemaFiles {
+		body, err := os.ReadFile(path + file)
+		if err != nil {
+			log.Fatalf("failed to run fixer: %s", err)
+		}
+
+		bytes.Replace(body, []byte(nodePt), []byte(""), -1)
+		bytes.Replace(body, []byte(nodesPt), []byte(""), -1)
+
+		rewrite, _ := os.OpenFile(path+file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		_, err = rewrite.Write(body)
+		if err != nil {
+			log.Fatalf("failed to run fixer: %s", err)
+		}
+		rewrite.Close()
+	}
+
+	log.Printf("fixer complite in: %s", path)
 }
